971c2aee4b4b87761d440acf59a216be3d2b3114
ycenta <thorbenliya@gmail.com>
2024-01-04 13:19:16 +0100

    add icons and remove lorem texts

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -44,31 +44,45 @@ onMounted(() => {
           <div class="bentoContainer div1">
             <h4 class="bentoTitle">Me</h4>
             <p>
-              lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> 
-              lorem ipsum dolor sit amet, consectetur adipiscing Nulla <br>
-              azeaez
+              Passionné par le dev web<br> 
+              Actuellement à la recherche d'une alternance en janvier 2024
             </p>
             <img src="/img/photo.jpg" height="100px" alt="bento" class="bentoImg">
           </div>
 
           <div class="bentoContainer div2">
-            <h4 class="bentoTitle">Langages, Frameworks...</h4>
-            <p>
-              lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> 
-              lorem ipsum dolor sit amet, consectetur adipiscing Nulla <br>
-              azeaez
-            </p>
-            <img src="/img/avatar.jpg" height="100px" alt="bento" class="bentoImg">
+            <h4 class="bentoTitle">Langages, Frameworks, Outils...</h4>
+            <div class="iconContainer">
+              <img src="/img/icons/laravel.png " height="70px" alt="bento">
+              <img src="/img/icons/react.png " height="70px" alt="bento">
+              <img src="/img/icons/vuejs.png " height="70px" alt="bento">
+              <img src="/img/icons/symfony.svg " height="70px" alt="bento">
+              <img src="/img/icons/rive.png " height="70px" alt="bento">
+              <img src="/img/icons/php.png" height="70px" width="70px" alt="bento">
+              <img src="/img/icons/js.png " height="70px" alt="bento">
+              <img src="/img/icons/css.png " height="70px" alt="bento">
+              <img src="/img/icons/c++.png " height="70px" alt="bento">
+              <img src="/img/icons/csharp.png " height="70px" alt="bento">
+              <img src="/img/icons/dockertrue.png " height="70px" width="70px" alt="bento">
+              <img src="/img/icons/strapi.png " height="70px" alt="bento">
+              <img src="/img/icons/git.png " height="70px" alt="bento">
+              <img src="/img/icons/postgresql.png " height="70px" alt="bento">
+              <img src="/img/icons/mysql.jpg " height="70px" alt="bento">
+            </div>
           </div>
 
           <div class="bentoContainer div3">
             <h4 class="bentoTitle">Créatif</h4>
-            <p>
-              lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> 
-              lorem ipsum dolor sit amet, consectetur adipiscing Nulla <br>
-              azeaez
-            </p>
-            <img src="/img/avatar.jpg" height="100px" alt="bento" class="bentoImg">
+            <div class="iconContainer">
+              <img src="/img/icons/ps.png " height="70px" alt="bento">
+              <img src="/img/icons/premierepro.png " height="70px" alt="bento">
+              <img src="/img/icons/ae.png " height="70px" alt="bento">
+              <img src="/img/icons/blender.png " height="70px" alt="bento">
+              <img src="/img/icons/unity.png " height="70px" alt="bento">
+              <img src="/img/icons/figma.webp " height="70px" alt="bento">
+           
+             
+            </div>
           </div>
 
 
@@ -77,18 +91,17 @@ onMounted(() => {
             <p>
               Mon profil <a href="https://github.com/ycenta">Github</a> <br>
             </p>
-            <img src="/img/avatar.jpg" height="100px" alt="bento" class="bentoImg">
+            <a href="https://github.com/ycenta"><img src="/img/icons/github.png" height="100px" alt="bento" class="bentoImg"></a>
           </div>
 
           <div class="bentoContainer div5">
             <h4 class="bentoTitle">Experience</h4>
             <p>
               <ul>
-                <li>2ans</li>
-                <li>lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li>2 ans+ en tant que developpeur web chez <b>CultureG</b></li>
+                <li>6 mois de bénévolat en tant qu'animateur (cours de programmation / 3D) </li>
               </ul>
             </p>
-            <img src="/img/avatar.jpg" height="100px" alt="bento" class="bentoImg">
           </div>
 
          
